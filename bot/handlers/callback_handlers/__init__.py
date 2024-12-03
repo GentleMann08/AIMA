8d26ff7627b171261ef0ebf1f4a8aa48b249b576
GentleMann2403 <Nick2403.bw@gmail.com>
2024-12-03 11:58:03 +0300

    Bonds subscription update

--- bot/handlers/callback_handlers/__init__.py
+++ bot/handlers/callback_handlers/__init__.py
@@ -1,11 +1,9 @@
 from .system_callbacks import system_router
 from .check_predictions import predictions_router
-from .subscribe_to_bonds import subscribe_router
-from .single_bond import choose_bond_router
+from .bonds import bonds_router
 
 callbacks_routers = [
     predictions_router,
     system_router,
-    subscribe_router,
-    choose_bond_router
+    bonds_router
 ]
---
+++ bot/handlers/callback_handlers/bonds.py
@@ -0,0 +1,71 @@
+from aiogram import Router, F
+from aiogram.fsm.context import FSMContext
+from aiogram.types import CallbackQuery
+from keyboards.menu import Menu
+
+bonds_router = Router()
+
+@bonds_router.callback_query(F.data == 'subscribe_to_bonds')
+async def all_bonds(callback_query: CallbackQuery, state: FSMContext):
+    data = await state.get_data()
+    last_message_id = data.get("last_message_id")
+
+    if last_message_id:
+        await callback_query.message.chat.delete_message(message_id=last_message_id)
+
+    keyboard = await Menu.to_bonds()
+    await callback_query.answer('Choose a bond to subscribe to')
+    new_message = await callback_query.message.answer(
+        text="Bonds (not all are currently supported):",
+        reply_markup=keyboard
+    )
+
+    await state.update_data(last_message_id=new_message.message_id)
+
+@bonds_router.callback_query(F.data.endswith('_bond'))
+async def single_bond(callback_query: CallbackQuery, state: FSMContext):
+    data = await state.get_data()
+    last_message_id = data.get("last_message_id")
+    bond = callback_query.data.split('_')[0]
+
+    if last_message_id:
+        await callback_query.message.chat.delete_message(message_id=last_message_id)
+
+    keyboard = await Menu.subscribe(bond_name=bond, state=state)
+    new_message = await callback_query.message.answer(
+        text=f"(here is {bond}'s description)",
+        reply_markup=keyboard
+    )
+
+
+    await state.update_data(last_message_id=new_message.message_id)
+    await state.update_data(last_message_text=new_message.text)
+
+@bonds_router.callback_query(F.data.endswith('_subscribe/unsubscribe'))
+async def subscribtion(callback_query: CallbackQuery, state: FSMContext):
+    data = await state.get_data()
+    last_message_id = data.get("last_message_id")
+    last_message_text = data.get("last_message_text")
+    subscriptions = data.get("subscriptions", [])
+
+    bond_name = callback_query.data.split('_')[0]
+
+    if bond_name not in subscriptions:
+        subscriptions = []
+        subscriptions.append(bond_name)
+        await callback_query.answer(f'You have subscribed to {bond_name.upper()}!')
+    else:
+        subscriptions.remove(bond_name)
+        await callback_query.answer(f'You have unsubscribed from {bond_name.upper()}!')
+    
+    if last_message_id:
+        await callback_query.message.chat.delete_message(message_id=last_message_id)
+
+    await state.update_data(subscriptions=subscriptions)
+    keyboard = await Menu.subscribe(bond_name=bond_name, state=state)
+    new_message = await callback_query.message.answer(
+        text=last_message_text,
+        reply_markup=keyboard
+    )
+
+    await state.update_data(last_message_id=new_message.message_id)
